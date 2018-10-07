4ca871b82a86b762825bd51b442b0764d9e38eaf
tiagojard <tiago.jardcarvalho@gmail.com>
2018-10-07 14:40:08 -0300

    dfgdfgfdg

--- src/server/index.js
+++ src/server/index.js
@@ -339,6 +339,7 @@ app.get("*", (req, res, next) => {
     </div>
 </div>
           <div id="app">${markup}</div>
+          <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
           <script>
           function EscondeLimparBusca(){
               $("#limpar-input-mobile").css("display","none")
